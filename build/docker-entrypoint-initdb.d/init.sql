e1dd43a7198fa4b6c96099df4b281e9827a81e94
azuki774 <azuki774s@gmail.com>
2022-11-24 01:15:21 +0900

    new db scheme

--- build/docker-entrypoint-initdb.d/init.sql
+++ build/docker-entrypoint-initdb.d/init.sql
@@ -1,108 +1,23 @@
--- MySQL dump 10.13  Distrib 8.0.3-rc, for Linux (x86_64)
---
--- Host: mawinter-db    Database: mawinter
--- ------------------------------------------------------
--- Server version	5.5.5-10.6.4-MariaDB-1:10.6.4+maria~focal
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-SET NAMES utf8mb4 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `categories`
---
+USE mawinter;
 grant all privileges on *.* to root@"%";
-DROP TABLE IF EXISTS `categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `type` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+
+CREATE TABLE `Category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category_id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `created_at` datetime  default current_timestamp,
+  `updated_at` timestamp default current_timestamp on update current_timestamp,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `category_id` (`category_id`),
-  KEY `type` (`type`),
-  CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`type`) REFERENCES `types` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE KEY `category_id` (`category_id`)
+);
 
---
--- Dumping data for table `categories`
---
-
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,100,'月給','income'),(2,101,'ボーナス','income'),(3,110,'雑所得','income'),(4,200,'家賃','outgoing'),(5,210,'食費','outgoing'),(6,220,'電気代','outgoing'),(7,221,'ガス代','outgoing'),(8,222,'水道費','outgoing'),(9,230,'コンピュータリソース','outgoing'),(10,231,'通信費（モバイル等）','outgoing'),(11,240,'生活用品','outgoing'),(12,250,'娯楽費','outgoing'),(13,251,'交遊費','outgoing'),(14,260,'書籍・勉強','outgoing'),(15,270,'交通費','outgoing'),(16,280,'衣服等費','outgoing'),(17,300,'保険・税金','outgoing'),(18,400,'医療・衛生','outgoing'),(19,500,'雑費','outgoing'),(20,600,'家賃用貯金','saving'),(21,601,'PC用貯金','saving'),(22,700,'NISA入出金','investment'),(23,701,'NISA変動','investment');
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `records`
---
-
-DROP TABLE IF EXISTS `records`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `records` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_id` int(11) NOT NULL,
-  `date` datetime DEFAULT current_timestamp(),
-  `price` int(11) NOT NULL,
-  `memo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+create table `Record_YYYYMM` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category_id` int NOT NULL,
+  `from` varchar(64) NOT NULL,
+  `type` varchar(64) NOT NULL,
+  `created_at` datetime  default current_timestamp,
+  `updated_at` timestamp default current_timestamp on update current_timestamp,
   PRIMARY KEY (`id`),
-  KEY `category_id` (`category_id`),
-  CONSTRAINT `records_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `records`
---
-
-LOCK TABLES `records` WRITE;
-/*!40000 ALTER TABLE `records` DISABLE KEYS */;
-/*!40000 ALTER TABLE `records` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `types`
---
-
-DROP TABLE IF EXISTS `types`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `types` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `type` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `types`
---
-
-LOCK TABLES `types` WRITE;
-/*!40000 ALTER TABLE `types` DISABLE KEYS */;
-INSERT INTO `types` VALUES (1,'income'),(4,'investment'),(2,'outgoing'),(3,'saving');
-/*!40000 ALTER TABLE `types` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
+  index `idx_cat` (`category_id`)
+);
