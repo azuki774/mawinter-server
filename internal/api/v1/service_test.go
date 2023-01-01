071d97a50e4df11ca930277f254f2dfe2e3301b3
azuki774 <azuki774s@gmail.com>
2023-01-01 16:38:48 +0900

    zap ts encoder

--- internal/api/v1/service_test.go
+++ internal/api/v1/service_test.go
@@ -6,17 +6,31 @@ import (
 	"mawinter-server/internal/model"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/go-sql-driver/mysql"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"gorm.io/gorm"
 )
 
 var l *zap.Logger
 
 func init() {
-	l, _ = zap.NewProduction()
+	config := zap.NewProductionConfig()
+	// config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	config.EncoderConfig.EncodeTime = JSTTimeEncoder
+	l, _ = config.Build()
+
+	l.WithOptions(zap.AddStacktrace(zap.ErrorLevel))
 }
+
+func JSTTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	const layout = "2006-01-02T15:04:05+09:00"
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	enc.AppendString(t.In(jst).Format(layout))
+}
+
 func Test_fyInterval(t *testing.T) {
 	type args struct {
 		yyyy int
