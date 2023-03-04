05703be9f61af4531984eb5ed2f0ee79270ebee1
azuki774 <azuki774s@gmail.com>
2023-03-04 12:47:05 +0900

    add e2e test temporary

---
+++ test/fixmonth_insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO Monthly_Fix_Billing VALUES (
+    1,
+    100,
+    5,
+    1000,
+    "S",
+    "memo",
+    now(),
+    now()
+);
+
+INSERT INTO Monthly_Fix_Billing VALUES (
+    2,
+    101,
+    15,
+    2000,
+    "S",
+    "memo",
+    now(),
+    now()
+);
