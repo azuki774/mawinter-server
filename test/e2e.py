7f49a3a70029069a79699afde29b7e48132f1925
azuki774 <azuki774s@gmail.com>
2023-03-07 22:51:46 +0900

    update e2e

--- test/e2e.py
+++ test/e2e.py
@@ -1,6 +1,7 @@
 # pip install mysql-connector-python
 import requests
 import sys
+import json
 import mysql.connector
 
 cnx = None
@@ -72,6 +73,7 @@ print('# create records')
 url = 'http://localhost:8080/v2/record'
 data1 = '{"category_id": 100, "datetime": "20000410", "from": "testfrom1", "type": "S1", "price": 1000, "memo": "memo"}'
 data2 = '{"category_id": 200, "datetime": "20000415", "from": "testfrom2", "type": "", "price": 2000, "memo": ""}'
+data3 = '{"category_id": 300, "datetime": "20000515", "from": "", "type": "", "price": 3000, "memo": ""}'
 headers = { "Content-Type": "application/json" }
 response = requests.post(url, data=data1, headers = headers)
 if response.status_code == 201:
@@ -89,6 +91,15 @@ else:
     print(response.status_code)
     sys.exit(1)
 
+response = requests.post(url, data=data3, headers = headers)
+if response.status_code == 201:
+    print("[OK] {}".format(url))
+else:
+    print("[NG] {}".format(url))
+    print(response.status_code)
+    sys.exit(1)
+
+
 print('# get records')
 url = 'http://localhost:8080/v2/record/200004'
 response = requests.get(url)
@@ -97,6 +108,23 @@ if response.status_code == 200:
     want = [{"category_id": 100, "category_name" : "月給", "id" : 1, "datetime": "2000-04-10T00:00:00+09:00", "from": "testfrom1", "type": "S1", "price": 1000, "memo": "memo"}, 
             {"category_id": 200, "category_name" : "家賃", "id" : 2, "datetime": "2000-04-15T00:00:00+09:00", "from": "testfrom2", "type": "", "price": 2000, "memo": ""}
            ]
+    if want != json_data:
+        print("[NG] {}".format(url))
+        print(json_data)
+        print(want)
+        sys.exit(1)
+    print("[OK] {}".format(url))
+else:
+    print("[NG] {}".format(url))
+    print(response.status_code)
+    sys.exit(1)
+
+print('# get year summary')
+url = 'http://localhost:8080/v2/record/summary/2000'
+response = requests.get(url)
+if response.status_code == 200:
+    json_data = response.json()
+    want = [{'category_id': 100, 'category_name': '月給', 'count': 1, 'price': [1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 1000}, {'category_id': 101, 'category_name': 'ボーナス', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 110, 'category_name': '雑所得', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 200, 'category_name': '家賃', 'count': 1, 'price': [2000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 2000}, {'category_id': 210, 'category_name': '食費', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 220, 'category_name': '電気代', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 221, 'category_name': 'ガス代', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 222, 'category_name': '水道費', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 230, 'category_name': 'コンピュータリソース', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 231, 'category_name': '通信費', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 240, 'category_name': '生活用品', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 250, 'category_name': '娯楽費', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 251, 'category_name': '交遊費', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 260, 'category_name': '書籍・勉強', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 270, 'category_name': '交通費', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 280, 'category_name': '衣服等費', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 300, 'category_name': '保険・税金', 'count': 1, 'price': [0, 3000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 3000}, {'category_id': 400, 'category_name': '医療・衛生', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 500, 'category_name': '雑費', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 600, 'category_name': '家賃用貯金', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 601, 'category_name': 'PC用貯金', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 700, 'category_name': 'NISA入出金', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}, {'category_id': 701, 'category_name': 'NISA変動', 'count': 0, 'price': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'total': 0}]
     if want != json_data:
         print("[NG] {}".format(url))
         print(json_data)
