308d7a2ad3474dae83872ae8cb19b8386d60796a
azuki774s <azuki774s@gmail.com>
2025-02-23 16:10:24 +0900

    show uri for logging

--- internal/server/server.go
+++ internal/server/server.go
@@ -65,7 +65,10 @@ func (s *Server) Start(ctx context.Context) error {
 func (s *Server) middlewareLogging(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
-			s.Logger.Info("access", zap.String("url", r.URL.Path), zap.String("X-Forwarded-For", r.Header.Get("X-Forwarded-For")))
+			s.Logger.Info("access", zap.String("uri", r.RequestURI), zap.String("X-Forwarded-For", r.Header.Get("X-Forwarded-For")))
+		} else {
+			// HealthCheck 用エンドポイントへのアクセスログはDebug
+			s.Logger.Debug("access", zap.String("uri", r.RequestURI), zap.String("X-Forwarded-For", r.Header.Get("X-Forwarded-For")))
 		}
 		h.ServeHTTP(w, r)
 	})
